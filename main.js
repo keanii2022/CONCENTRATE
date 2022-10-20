fc27f26e27de55128f44340dc250cf36e74dc9e3
keanii2018 <keani.mpcc@outlook.com>
2022-10-20 14:40:02 -0700

    create the board/verified in chrome dev tools

--- main.js
+++ main.js
@@ -25,24 +25,48 @@ document.addEventListener('DOMContentLoaded', () => {
             img:""
         },
         {
-            name:'card-7',
+            name:'card-1',
             img:""
         },
         {
-            name:'card-8',
+            name:'card-2',
             img:""
         },
         {
-            name:'card-9',
+            name:'card-3',
             img:""
         },
         {
-            name:'card-10',
+            name:'card-4',
             img:""
         },
         {
-            name:'card-11',
+            name:'card-5',
             img:""
-        }
+        },
+        {
+            name:'card-6',
+            img:""
+        },
     ]
+    cardArray.sort(() => 0.5 - Math.random())
+    
+    const board = document.querySelector('.board')
+    const score = document.getElementById('score')
+    const placeholder = ""
+
+    var cardsClicked = []
+    var cardsClickedId = []
+    var cardsMatches = []
+
+    // Create the board
+    function createBoard() {
+        for(let i=0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', placeholder)
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            board.appendChild(card)
+        }
+    }
 })
