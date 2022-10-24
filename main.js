7836b0b54d257d9eedf85274978962bc2c7f0a7f
keanii2018 <keani.mpcc@outlook.com>
2022-10-24 12:31:37 -0700

    matchCard function added/need to add 100x100 pictures

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         {
             name:'card-1',
-            img:""
+            img:"https://i.imgur.com/fDstWiz.png"
         },
         {
             name:'card-2',
-            img:""
+            img:"https://i.imgur.com/t6ZL26V.jpg"
         },
         {
             name:'card-3',
@@ -26,11 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name:'card-1',
-            img:""
+            img:"https://i.imgur.com/fDstWiz.png"
         },
         {
             name:'card-2',
-            img:""
+            img:"https://i.imgur.com/t6ZL26V.jpg"
         },
         {
             name:'card-3',
@@ -52,10 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
     cardArray.sort(() => 0.5 - Math.random())
     
     const board = document.querySelector('.board')
+    const attempts = document.getElementById('attempts')
     const placeholder = "https://loremflickr.com/100/100"
+    const blank = ""
 
     var cardsClicked = []
     var cardsClickedId = []
+    var cardMatches = []
 
     // Create the board
     function createBoard() {
@@ -76,6 +79,38 @@ document.addEventListener('DOMContentLoaded', () => {
         this.setAttribute('src', cardArray[cardId].img)
         if (cardsClicked.length ===2) {
             setTimeout(checkForMatch, 400)
+        }
+    }
+
+    // checkForMatch function
+    function checkForMatch() {
+        var cards = document.querySelectorAll;('img')
+        const firstCard = cardsClickedId[0]
+        const secondCard = cardsClickedId[1]
+    
+        if(firstCard === secondCard) {
+            cards[firstCard].setAttribute('src', placeholder)
+            cards[secondCard].setAttribute('src', placeholder)
+            alert('GOOD JOB DUDE!')
+        }
+        else if (cardsClicked[0] === cardsClicked [1]) {
+            cards[firstCard].setAttribute('src', blank)
+            cards[secondCard].setAttribute('src', blank)
+            cards[firstCard].removeEventListener('click', flipCard)
+            cards[secondCard].removeEventListener('click', flipCard)
+            cardsMatched.push(cardsClicked)
+        }
+        else {
+            setTimeout(() => {
+                cards[firstCard].setAttribute('src', placeholder)
+                cards[secondCard].setAttribute('src', placeholder)
+            }, 350)
+        }
+        cardsClicked = []
+        cardsClickedId = []
+        result.textContent = cardsMatched.length 
+        if (cardsMatched.length === cardArray.length/2) {
+            result.textContent = 'YOOO YOU WON'
         }
     }
 
